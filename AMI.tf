0d33d8f718935b6a28e6b366905cdf9d0f6f70d3
gaddamchandrakanth <gaddamchandrakanth1995@gmail.com>
2025-09-09 15:32:54 +0530

    Create AMI.tf

---
+++ AMI.tf
@@ -0,0 +1,72 @@
+# Ubuntu
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Amazon Linux
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+
+# Red Hat (RHEL)
+data "aws_ami" "rhel" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-9.*_HVM-*x86_64-*"]
+  }
+}
+
+
+# Windows Server
+data "aws_ami" "windows" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-*-English-Full-*"]
+  }
+  filter {
+    name   = "platform"
+    values = ["windows"]
+  }
+}
+
+
+# SUSE (BYOS example)
+data "aws_ami" "suse" {
+  most_recent = true
+  owners      = ["013907871322"]
+  filter {
+    name   = "name"
+    values = ["suse-sles-*-hvm-ssd-x86_64"]
+  }
+}
+
+
+
+# Debian
+data "aws_ami" "debian" {
+  most_recent = true
+  owners      = ["136693071363"]
+  filter {
+    name   = "name"
+    values = ["debian-*-*-*-amd64-*"]
+  }
+}
